a181a0e8f0c99edbfd7356b6ea563b6a0e935e33
Panos Karagiannis <pkaragia@ucsc.edu>
2017-11-11 18:03:36 -0800

    added weighted (multiplicity) functionality

--- e2e_nlg/weed_out_ref.py
+++ e2e_nlg/weed_out_ref.py
@@ -2,8 +2,10 @@ import nltk
 import heapq
 from collections import Counter
 import pandas as pd
+from math import ceil
 
-def eval_ref(group):		
+def eval_ref(group, n):	
+	''' Evaluate a group of references and return the n best '''	
 	score_tracker = []
 
 	for line in group['ref']:
@@ -32,17 +34,16 @@ def eval_ref(group):
 		heapq.heappush(score_tracker, (-avg_score, line))
 
 	#return n smallest since we save the negative of the score since this is a min heap
-	return heapq.nsmallest(2, score_tracker)
+	return heapq.nsmallest(n, score_tracker)
 			
 		
-def keep_the_best(df):
+def keep_the_best(df,n):
+	''' Keeps n best references after evaluation '''
 	new_df =  pd.DataFrame(columns=['mr', 'ref'])
 	for name, group in df.groupby('mr'):
 		#print(name)
 
-		n_best = eval_ref(group)
-
-		
+		n_best = eval_ref(group, n)
 		s=''
 		for element in n_best:
 			#s = s + "S: "+ str(-element[0])+", "+element[1]+'\n'
@@ -51,8 +52,46 @@ def keep_the_best(df):
 			new_df = new_df.append(temp_df)
 		#print(s+'\n')
 
-	new_df.to_csv('train_2best.csv', index=False)
-	print(new_df)
+	new_df.to_csv('train_2best.csv', index=False, encoding='utf-8')
+
+def keep_the_best_weighted(df, weight, n):
+	''' Increases the weight of the n best references. 
+		Note that the weight means that an extra amount of weight many
+		instances will be added in addition to the existing ones 
+	'''
+
+	new_df =  pd.DataFrame(columns=['mr', 'ref'])
+	for name, group in df.groupby('mr'):
+
+		n_best = eval_ref(group, n)
+		new_df = new_df.append(group) #append existing
+
+		#TO DO: make this more generic for n best not just 2
+		first_best_score = - n_best[0][0]
+		second_best_score = -n_best[1][0]
+
+		# if there is a clear winner
+		if first_best_score > second_best_score:
+
+			for i in range(weight):
+				new_pair = {'mr': [name], 'ref': [n_best[0][1]]}
+				temp_df = pd.DataFrame(data=new_pair, dtype=str)
+				new_df = new_df.append(temp_df)
+		else:
+			for i in range(ceil(weight/n)):
+				new_pair = {'mr': [name], 'ref': [n_best[0][1]]} #this has to change if n != 2
+				temp_df = pd.DataFrame(data=new_pair, dtype=str)
+				new_df = new_df.append(temp_df)
+
+			for i in range(ceil(weight/n), weight):
+				new_pair = {'mr': [name], 'ref': [n_best[1][1]]}
+				temp_df = pd.DataFrame(data=new_pair, dtype=str)
+				new_df = new_df.append(temp_df)
+
+	new_df.to_csv('data/train_2best_weight.csv', index=False, encoding='utf-8')
+
+
+
 
 
 def main():
@@ -65,7 +104,8 @@ def main():
 	df['mr']=df['mr'].astype('str')
 	df['ref']=df['ref'].astype('str')
 
-	keep_the_best(df)
+	#keep_the_best(df, n=2)
+	keep_the_best_weighted(df, weight = 5, n = 2)
 
 
 
