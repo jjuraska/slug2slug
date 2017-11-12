b9576b1f2d0a4333a6e4812a0a39422124a0247b
Panos Karagiannis <pkaragia@ucsc.edu>
2017-11-12 15:42:09 -0800

    added stats printout

--- e2e_nlg/compare_references.py
+++ e2e_nlg/compare_references.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from math import ceil
+import subprocess
 
 def print_group(g1, g2):
 	s= ''
@@ -23,9 +24,25 @@ def compare(df1, df2):
 				break
 
 	#print stats
-	print("# of total differecnes = ", str(diff_counter)+ " (" + str( ceil(diff_counter/len(df1) * 100)) + '%)' )
+	print("# of total (group) differecnes = ", str(diff_counter)+ " (" + str( ceil(diff_counter/len(df1) * 100)) + '%)' )
+
+def print_stats(file1, file2):
+	p1_and = subprocess.Popen(["grep", "-c", "and", file1], stdout=subprocess.PIPE)
+	p2_and = subprocess.Popen(["grep", "-c", "and", file2], stdout=subprocess.PIPE)
+
+	p1_but = subprocess.Popen(["grep", "-c", "but", file1], stdout=subprocess.PIPE)
+	p2_but = subprocess.Popen(["grep", "-c", "but", file2], stdout=subprocess.PIPE)
 
 
+	out1_and, err = p1_and.communicate()
+	out2_and, err = p2_and.communicate()
+
+	out1_but, err = p1_but.communicate()
+	out2_but, err = p2_but.communicate()
+
+
+	print("Number of 'and' keyword: %s - %s = %d" %(file1, file2, int(out1_and)-int(out2_and)) )
+	print("Number of 'but' keyword: %s - %s = %d" %(file1, file2, int(out1_but)-int(out2_but)) )
 
 def main():
 	####### SET ##########
@@ -49,6 +66,7 @@ def main():
 		print("Cannot compare files of different sizes.")
 	else:
 		compare(df1, df2)
+		print_stats(new_file, old_file)
 
 
 if __name__ == '__main__':
